94eea11e9c3c68e9539a65a09bb379ecbacdc257
alztrk <agnes091241856326@gmail.com>
2026-06-24 02:21:34 +0300

    Fix base path for project Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gitradar/',
   build: { outDir: 'docs' },
   plugins: [react(), tailwindcss()],
 })
